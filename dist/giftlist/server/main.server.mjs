8cc467876ef064bea557fcee7e450159e5c6bf58
Thiago Costa <tcaa@icomp.ufam.edu.br>
2024-03-25 21:15:57 -0400

    build

--- dist/giftlist/server/main.server.mjs
+++ dist/giftlist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4P6SVFAZ.mjs";import"./chunk-W37SXZFE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5LCLQUSV.mjs";import"./chunk-DMDP7ZDH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
