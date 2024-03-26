48875259084a373fa089aa7b83a953fe9b1c08f4
Thiago Costa Antunes Afonso (SIDI) <t.afonso@samsung.com>
2024-03-25 20:30:17 -0400

    adding gifts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,95 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
 	giftlist = [
 		{
-			img: "assets/emerson-vieira-3wzps8KbdCQ-unsplash.jpg",
-			description: "Conjunto de utensílios de cozinha de macho editado de novo: Para transformar sua cozinha em um arco-íris culinário - afinal, cozinhar fica muito mais divertido quando você tem uma colher de pau rosa choque!",
-			price: "R$ 46,37"
+      img: "assets/cafeteira.jpg",
+			description: "Máquina de Café Expresso pra fazer hora extra",
+			price: "R$ 400,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/6602125e-26a5-4982-a3e4-f106701a2338"
 		},
 		{
-			img: "assets/emerson-vieira-3wzps8KbdCQ-unsplash.jpg",
-			description: "Jogo de facas de chef de alta qualidade: Porque cortar vegetais não precisa ser uma batalha épica digna de um filme de ação. Prepare-se para fatiar e picar como um verdadeiro mestre ninja da culinária!",
-			price: "R$ 46,37"
+      img: "assets/cortina.jpg",
+			description: "Cortina da sala pra não pagar peitinho pros vizinhos",
+			price: "R$ 128,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/660212c2-c2dd-46b2-bbf7-c1aede45298a"
 		},
 		{
-			img: "assets/emerson-vieira-3wzps8KbdCQ-unsplash.jpg",
-			description: "Tapete de entrada personalizado: Dê as boas-vindas aos seus convidados com um tapete que grita Entre por sua própria conta e risco - brindes iincluem piadas ruins e café fresco.",
-			price: "R$ 46,37"
+      img: "assets/ferramentas.jpg",
+			description: "Kit de Ferramentas pra não depender do marido de aluguel",
+			price: "R$ 153,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021304-58e5-431b-8ff9-47259dac34f2"
 		},
 		{
-			img: "assets/emerson-vieira-3wzps8KbdCQ-unsplash.jpg",
-			description: "Conjunto de toalhas de mesa e guardanapos: Para aqueles momentos em que as refeições ficam um pouco... desajeitadas. Afinal, não há vergonha em ter um guardanapo pronto para a batalha!",
-			price: "R$ 46,37"
+      img: "assets/cama.jpg",
+			description: "Conjunto de cama pras visitas não morrerem de frio",
+			price: "R$ 67,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/6602134c-0cc2-43c6-901f-79ec51371830"
 		},
-		{
-			img: "assets/emerson-vieira-3wzps8KbdCQ-unsplash.jpg",
-			description: "esse é um presente inesquecível",
-			price: "R$ 46,37"
-		}
+    {
+      img: "assets/potes.jpg",
+			description: "Kit tupperware pra fazer o rancho em dia de festa do sidi",
+			price: "R$ 51,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021381-80e4-44de-9f0f-0d7a54ece94f"
+		},
+    {
+      img: "assets/cadeira.jpg",
+      description: "Cadeira de escritório pra não ficar com dor nas costas",
+      price: "R$ 280,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/660213e7-8f1b-4f18-a643-71e16ce5a615"
+    },
+    {
+      img: "assets/relogio.jpg",
+      description: "Relógio de parede pra não perder a hora da daily",
+      price: "R$ 46,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/6602141c-225f-46c5-9005-6c982ec9612a"
+    },
+    {
+      img: "assets/planta.jpg",
+      description: "Planta artificial porque a gente não tem tempo pra cuidar",
+      price: "R$ 35,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021464-6128-4c75-8851-d3e269652396"
+    },
+    {
+      img: "assets/retrato.jpg",
+      description: "Porta retratos pra não esquecer a familia sifex",
+      price: "R$ 80,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021491-3997-40ce-816e-2d76d717d4b5"
+    },
+    {
+      img: "assets/roteador.jpg",
+      description: "Um mês de fibra otica porque depois disso tu se vira",
+      price: "R$ 100,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/660214db-ac71-469f-9e8e-e946b26afe14"
+    },
+    {
+      img: "assets/vinho.jpg",
+      description: "Kit de vinhos pra receber os amigos",
+      price: "R$ 202,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021502-05fb-49f2-822d-dc0b76b783d3"
+    },
+    {
+      img: "assets/luminario.jpg",
+      description: "Luminária de mesa pra trabalhar depois do horário",
+      price: "R$ 97,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/6602152b-81e7-4aeb-be1a-5cfc52ebfecc"
+    },
+    {
+      img: "assets/ventilador.jpg",
+      description: "Ventilador para os dias quentes",
+      price: "R$ 141,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021561-e184-4f0c-b766-761341290a93"
+    },
+    {
+      img: "assets/sad.jpg",
+      description: "Nem gosto tanto assim de ti...",
+      price: "R$ 1,99",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/66021589-57d2-4766-a1a2-2229d8dea3cd"
+    },
+    {
+      img: "assets/panelas.jpg",
+			description: "Jogo de Panelas Tramontina (Vale 01 Jantar Romântico no Apartamento)",
+			price: "R$ 515,00",
+      pixurl: "https://nubank.com.br/cobrar/q8h3/660215b4-1eaf-498f-ba74-e9a06e26c694"
+		},
 	]
 
 }
